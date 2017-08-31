dfa819c9f6d576898b076946576a62a3b51ba9f5
Phil Barker <phil@pjjk.net>
2017-08-31 11:22:28 +0100

    added quick option, with laxer speedier file comparison before deciding whether to copy when a file already exists in target

--- musicsync.py
+++ musicsync.py
@@ -25,7 +25,7 @@ class Playlist(list):
             print('INFO: closing playlist file.')
             infile.close()
 
-    def _copyfile(self, ifn, ofn):
+    def _copyfile(self, ifn, ofn, q):
         if not os.path.exists(os.path.dirname(ofn)):
             try:
                 print('INFO: making destination folder',
@@ -35,22 +35,31 @@ class Playlist(list):
                 if exc.errno != errno.EEXIST:
                     raise
         if os.path.exists(ofn):
-            if compare(ifn, ofn):  # do nothing if output file already same as input
-                print('INFO: file already exists. %s' % ofn)
+            if q: # skip if file exists with same name 
+                print('INFO: file with name already exists %s' % ofn)
                 return
-            elif (os.path.getmtime(ifn) < os.path.getmtime(ofn)):
-                print('INFO: not copying over newer file.')
-                print('INFO: %s is newer than %s' % (ofn, ifn))
-            else:
-                pass
+            else:  # check file with same name, copy if source is diff & newer
+                if compare(ifn, ofn):
+                    # do nothing if output file already same as input
+                    print('INFO: file already exists %s' % ofn)
+                    return
+                elif (os.path.getmtime(ifn) < os.path.getmtime(ofn)):
+                    print('INFO: not copying over newer file.')
+                    print('INFO: %s is newer than %s' % (ofn, ifn))
+                else:
+                    pass
         else:
             try:
                 print('INFO: Copying %s \n\t to %s' % (ifn, ofn))
                 copy2(ifn, ofn)
-            except:
-                print('WARNING: could not copy %s \n\t to %s' % (ifn, ofn))
+            except Exception as e:
+                if e.errno == 95: # raised becaused permission cannot be set 
+                    pass
+                else:
+                    print('WARNING: could not copy %s \n\t to %s' % (ifn, ofn))
+                    print(str(e))
 
-    def copyfiles(self, target, source, first=1, last=0):
+    def copyfiles(self, target, source, q, first=1, last=0):
         ofn = str('')         # output file name
         ifn = str('')         # input file name
         discard = len(source)
@@ -61,7 +70,7 @@ class Playlist(list):
                 for ifn in self[first:last]:
                     if ifn.startswith(source):
                         ofn = target + ifn[discard:] # strips source directory
-                        self._copyfile(ifn, ofn)
+                        self._copyfile(ifn, ofn, q)
                     else:
                         print('INFO: file is not in source directory.')
                         print('INFO: skipping file %s.' % ifn)
@@ -87,19 +96,31 @@ class Playlist(list):
             common_start = ''.join(_iter(self[i], common_start))
         return common_start
         
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1', 'quick'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='./musicsync.py',
+    parser = argparse.ArgumentParser(prog='musicsync.py',
                 description='sync music from library to device using an m3u playlist')
     parser.add_argument('-if', '--infile', type=str, default='playlist.m3u',
                         metavar='<file name>',
                         help='input file, an m3u playlist, defaults to playlist.m3u')
     parser.add_argument('-t', '--target', type=str, default='./Copiedfiles/',
-                        metavar='<directory name>',
+                        metavar='<directory path>',
                         help='target, path to a directory defaults to ./Copiedfiles/')
     parser.add_argument('-s', '--source', type=str, 
-                        metavar='<directory name>',
-                        help='source, path to a directory, a good default is guessed')
+                        metavar='<directory path>',
+                        help='source directory, a good default is guessed')
+    parser.add_argument('-q', '--quick',type=str2bool, nargs='?',
+                        const=True, default=False, metavar='True',
+                        help='Do only a quick check for existing files')
     args = parser.parse_args()
 
     infile = args.infile
@@ -116,8 +137,11 @@ if __name__ == "__main__":
             source = args.source + '/'
     else:
         source = playlist.guess_source()
+    quick = args.quick
+    if quick:
+        print('INFO: quick option selected')
+        print('\tWill skip copying if file of name name exists on device')
+        print('\teven if it is different from source file.')
     print('INFO: source is %s,' % source)
-    print('INFO: path to source will be ignored when creating sub directories in target')
-
-
-    playlist.copyfiles(target, source, 0, 10)
+    print('\tpath to source will be ignored when creating sub directories in target')
+    playlist.copyfiles(target, source, quick, 0, 10)
